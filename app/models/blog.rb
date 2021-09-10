1c867ea502ef99a10b6f94110000b68976a2badb
Cameron Lund <clund6314@gmail.com>
2021-09-09 21:18:26 -0600

    added has_many and validations

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,3 +1,5 @@
 class Blog < ApplicationRecord
   belongs_to :user
+  has_many :posts, dependent: :destroy
+  validates :title, :category, presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,29 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_09_013857) do
+ActiveRecord::Schema.define(version: 2021_09_10_030014) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "blogs", force: :cascade do |t|
+    t.string "title"
+    t.string "category"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_blogs_on_user_id"
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.string "author"
+    t.text "body"
+    t.bigint "blog_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["blog_id"], name: "index_posts_on_blog_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "provider", default: "email", null: false
@@ -45,4 +64,6 @@ ActiveRecord::Schema.define(version: 2021_09_09_013857) do
     t.index ["uid", "provider"], name: "index_users_on_uid_and_provider", unique: true
   end
 
+  add_foreign_key "blogs", "users"
+  add_foreign_key "posts", "blogs"
 end
