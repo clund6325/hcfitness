8179c75373ff54621d2534e24881b179532907b8
Cameron Lund <clund6314@gmail.com>
2021-09-14 21:55:59 -0600

    added content to register.js

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -3,5 +3,73 @@ import { AuthConsumer } from '../../providers/AuthProvider';
 import { Button, Form } from 'react-bootstrap';
 
 const Register = ({ handleRegister, history }) => {
-  const [user, setUser] = useState({email: "", password: "", name: "", })
+  const [user, setUser] = useState({email: "", password: "", name: "", image: "", passwordConfirmation: "" })
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    if (user.password === user.passwordConfirmation){
+      handleRegister(user, history)
+      setUser({email: "", password: "", name: "", image: "", passwordConfirmation: ""})
+    }
+    else{
+      alert("Passwords do not match")
+    }
+  }
+
+  return(
+    <>
+      <h1>Register</h1>
+      <Form onSubmit={handleSubmit}>
+        <Form.Group controlId="formBasicName">
+          <Form.Label>Name</Form.Label>
+          <Form.Control 
+            type="text"
+            placeholder="enter name"
+            name="name"
+            value={user.name}
+            onChange={(e) => setUser({...user, name: e.target.value})}
+          />
+        </Form.Group>
+        <Form.Group controlId="formBasicImage">
+          <Form.Label>Image</Form.Label>
+          <Form.Control
+            type="text"
+            placeholder="enter image url"
+            name="image"
+            value={user.image}
+            onChange={(e) => setUser({...user, image: e.target.value})}
+          />
+        </Form.Group>
+        <Form.Group controlId="formBasicPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control
+            type="password"
+            placeholder="enter password"
+            name="password"
+            value={user.password}
+            onChange={(e) => setUser({...user, password: e.target.value})}
+          />
+        </Form.Group>
+        <Form.Group controlId="formBasicPasswordConfirmation">
+          <Form.Label>Password Confirmation</Form.Label>
+          <Form.Control
+            type="password"
+            placeholder="password confirmation"
+            name="passwordConfirmation"
+            value={user.passwordConfirmation}
+            onChange={(e) => setUser({...user, passwordConfirmation: e.target.value})}
+          />
+        </Form.Group>
+        <Button variant="primary" type="submit">Submit</Button>
+      </Form>
+    </>
+  )
 }
+
+const ConnectedRegister = (props) => (
+  <AuthConsumer>
+    { auth => <Register {...props} {...auth} /> }
+  </AuthConsumer>
+)
+
+export default ConnectedRegister;
