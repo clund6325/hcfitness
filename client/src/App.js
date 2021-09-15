c7c73d427d37ccdff2df88486f6719b5360807bb
Cameron Lund <clund6314@gmail.com>
2021-09-14 20:37:18 -0600

    filled out app js. created blogs folder and files

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import { Switch, Route } from 'react-router-dom';
+import { Container } from 'react-bootstrap';
+import Home from './components/shared/Home';
+import Nomatch from './components/shared/Nomatch';
+import Login from './components/auth/Login';
+import Register from './components/auth/Register';
+import MainNavbar from './components/shared/MainNavbar';
+import Blogs from './components/blogs/Blogs';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => (
+  <>
+    <MainNavbar />
+    <Container>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/login" component={Login} />
+        <Route exact path="/register" component={Register} />
+        <Route exact path="/blogs" component={Blogs} />
+        <Route component={Nomatch} />
+      </Switch>
+    </Container>
+  </>
+)
 
 export default App;
