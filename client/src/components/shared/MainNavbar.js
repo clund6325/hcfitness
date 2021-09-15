acbf999ceeefc149d78e6edc5d944b40fd9130c4
Cameron Lund <clund6314@gmail.com>
2021-09-14 21:09:13 -0600

    added content to MainNavbar.js

---
+++ client/src/components/shared/MainNavbar.js
@@ -0,0 +1,40 @@
+import { AuthConsumer } from '../../providers/AuthProvider';
+import { Link, withRouter } from 'react-router-dom';
+import { Navbar, Nav, Button } from 'react-bootstrap';
+
+const MainNavbar = ({ user, handleLogout, location, history }) => {
+  const rightNavItems = () => {
+    if (user) {
+      return(
+        <Nav className="justify-content-end">
+          <Button variant="outline-info" onClick={() => handleLogout(history)}>Logout</Button>
+        </Nav>
+      )
+    }
+    else{
+      return(
+        <Nav className="mr-auto justify-content-end">
+          <Nav.Link href="/home">Home</Nav.Link>
+          <Nav.Link href="/about">About</Nav.Link>
+          <Nav.Link href="/blogs">Blogs</Nav.Link>
+        </Nav>
+      )
+    }
+  }
+  return(
+    <Navbar bd="dark" variant="dark">
+      <Link to="/">
+        <Navbar.Brand>Home</Navbar.Brand>
+      </Link>
+      { rightNavItems() }
+    </Navbar>
+  )
+}
+
+const ConnectedMainNavbar = (props) => (
+  <AuthConsumer>
+    { auth => <MainNavbar {...props} {...auth} /> }
+  </AuthConsumer>
+)
+
+export default withRouter(ConnectedMainNavbar);
