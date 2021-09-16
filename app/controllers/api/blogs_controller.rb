4a6b09b50b43924b0da1f8f83294efe7c4627e46
Cameron Lund <clund6314@gmail.com>
2021-09-15 21:34:27 -0600

    coded blogs controller

--- app/controllers/api/blogs_controller.rb
+++ app/controllers/api/blogs_controller.rb
@@ -1,2 +1,42 @@
 class Api::BlogsController < ApplicationController
+  before_action :set_blog, only: [:show, :update, :destroy]
+
+  def index
+    render json: current_user.blogs
+  end
+
+  def show
+    render json: @blog
+  end
+
+  def create
+    @blog = current_user.blogs.new(blog_params)
+    if @blog.save
+      render json: @blog
+    else
+      render json: { errors: @blog.errors}, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    if @blog.update(blog_params)
+      render json: @blog
+    else
+      render json: { errors: @blog.errors}, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @blog.destroy
+    render json: { message: "Blog Deleted"}
+  end
+
+  private
+    def blog_params
+      params.require(:blog).permit(:title, :category, :image)
+    end
+
+    def set_blog
+      @blog = current_user.blogs.find(params[:id])
+    end
 end
