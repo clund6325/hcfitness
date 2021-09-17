ad449222e3711d7638ec466352771752d5b63aa1
Cameron Lund <clund6314@gmail.com>
2021-09-16 21:04:15 -0600

    coded blogprovider.js

---
+++ client/src/providers/BlogProvider.js
@@ -0,0 +1,66 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+
+export const BlogContext = React.createContext();
+export const BlogConsumer = BlogContext.Consumer;
+
+const BlogProvider = ({ children }) => {
+  const [blogs, setBlogs] = useState([])
+
+  const getAllBlogs = () => {
+    axios.get('/api/blogs')
+      .then( res => {
+        setBlogs(res.data)
+      })
+      .catch( err => console.log(err))
+  }
+
+  const addBlog = ( blog, history ) => {
+    axios.post('/api/blogs', { blog })
+      .then( res => {
+        setBlogs([...blogs, res.data])
+      })
+      .catch( err => console.log(err))
+      history.push("/blogs")
+  }
+
+  const updateBlog = ( id, blog, history ) => {
+    axios.put(`/api/blogs/${id}`, { blog })
+      .then( res => {
+        const updatedBlogs = blogs.map( b => {
+          if ( b.id === id ) {
+            return res.data
+          }
+          return b
+        })
+        setBlogs(updateBlogs)
+        history.push("/blogs")
+      })
+      .catch( err => console.log(err))
+      history.push("/blogs")
+  }
+
+  const deleteBlog = ( id, history ) => {
+    axios.delete(`/api/blogs/${id}`)
+      .then( res => {
+        setBlogs(blogs.filter(b => b.id !== id))
+        alert(res.data.message)
+        history.push("/blogs")
+      })
+      .catch( err => console.log(err))
+  }
+
+  return(
+    <BlogContext.Provider value={{
+      plans,
+      getAllBlogs: getAllBlogs,
+      addBlog: addBlog,
+      updateBlog: updateBlog,
+      deleteBlog: deleteBlog
+    }}>
+      { children }
+    </BlogContext.Provider>
+  )
+}
+
+export default BlogProvider;
