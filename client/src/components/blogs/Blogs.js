118d925d59f8c3097d8c5181c8f45ee349691669
Cameron Lund <clund6314@gmail.com>
2021-09-20 16:44:36 -0600

    added bloglist to blogs page

--- client/src/components/blogs/Blogs.js
+++ client/src/components/blogs/Blogs.js
@@ -1,10 +1,14 @@
 import { Jumbotron } from 'react-bootstrap';
+import BlogList from './BlogList';
 
 const Blogs = () => {
   return(
-    <Jumbotron>
-      <h1>Blogs Page</h1>
-    </Jumbotron>
+    <>
+      <Jumbotron>
+        <h1>Blogs Page</h1>
+      </Jumbotron>
+      <BlogList/>
+    </>
   )
 }
 
