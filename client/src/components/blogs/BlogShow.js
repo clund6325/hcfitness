700bfce5448c138dddb18925e1ab35f183b442b9
Cameron Lund <clund6314@gmail.com>
2021-09-20 20:51:53 -0600

    added content to the blog show page

---
+++ client/src/components/blogs/BlogShow.js
@@ -0,0 +1,41 @@
+import { Button, Modal } from 'react-bootstrap';
+import { useState } from 'react';
+import BlogForm from './BlogForm';
+import { BlogConsumer } from '../../providers/BlogProvider';
+
+const BlogShow = ({ location, match, deleteBlog, history }) => {
+const [editshow, setEditShow] = useState(false);
+
+const handleEditClose = () => setEditShow(false);
+const handleEditShow = () => setEditShow(true);
+
+  return(
+    <>
+      <h1>{location.state.title}</h1>
+      <p>Category: {location.state.category}</p>
+      {''}
+      <Button variant="warning" onClick={() => handleEditShow()}>Edit</Button>
+      {''}
+      <Button variant="danger" onClick={() => deleteBlog(match.params.id, history)}>Delete</Button>
+      <Modal show={editshow} onHide={handleEditClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Blog Title: {location.state.title} Edit</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <BlogForm {...location.state } handleEditClose={handleEditClose}/>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleEditClose}>Close</Button>
+        </Modal.Footer>
+      </Modal>
+    </>
+  )
+}
+
+const ConnectedBlogShow = (props) => (
+  <BlogConsumer>
+    { value => <BlogShow {...props} {...value}/> }
+  </BlogConsumer>
+)
+
+export default ConnectedBlogShow;
